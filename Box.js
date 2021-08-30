119f4d46f47884cc71fabc162f637ac65f711203
christinawondwessen <78184033+christinawondwessen@users.noreply.github.com>
2021-08-30 18:03:10 -0400

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,15 @@
+class Box extends BaseClass {
+    constructor(x, y, width, height){
+      super(x,y,width,height);
+      //this.image = loadImage("sprites/wood1.png");
+    }
+    
+    visibility(){
+    this.visibility===5;
+     if(speed<3){
+         display === true;
+     }
+    }
+
+  };
+  
